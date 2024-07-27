f9d882da7ead2f9b53b93a0f31ff01469a79b1f6
AidenKerr <arkerr2000@gmail.com>
2024-07-27 10:21:16 -0700

    add more simple routes

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,11 @@ let logger = require('morgan');
 let indexRouter = require('./routes/index');
 let usersRouter = require('./routes/users');
 let animalsRouter = require('./routes/animals');
+let equipmentRouter = require('./routes/equipment');
+let facilitiesRouter = require('./routes/facilities');
+let habitatsRouter = require('./routes/habitats');
+let plantsRouter = require('./routes/plants');
+let rangersRouter = require('./routes/rangers');
 
 let app = express();
 
@@ -18,5 +23,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/animals', animalsRouter);
+
+app.use('/equipment', equipmentRouter);
+app.use('/facilities', facilitiesRouter);
+app.use('/habitats', habitatsRouter);
+app.use('/plants', plantsRouter);
+app.use('/rangers', rangersRouter);
 
 module.exports = app;
